60343a148e0b18e38b2b82650a874667b91b2b42
Ravel Costa <ravelcostadearaujo@gmail.com>
2026-01-08 23:52:20 -0300

    Add códigos

--- logtree.c
+++ logtree.c
@@ -1,4 +1,93 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "logtree.h"
+
+typedef struct log {
+    int valor;
+    int classe;
+    int tempo;
+    int caixa;
+
+    struct log *esq;
+    struct log *dir;
+} Log;
+
+void inicializar(Log **l) {
+    *l = NULL;
+}
+
+Log* criar_no(int conta, int classe, int tempo, int caixa) {
+    Log *novo = malloc(sizeof(Log));
+
+    if (novo == NULL) return NULL;
+
+    novo->conta = conta;
+    novo->classe = classe;
+    novo->tempo = tempo;
+    novo->caixa = caixa;
+    novo->esq = NULL;
+    novo->dir = NULL;
+
+    return novo;
+}
+
+void log_registrar(Log **l, int conta, int classe, int tempo, int caixa) {
+    if (*l == NULL) {
+        *l = criar_no(conta, classe, tempo, caixa);
+        return;
+    }
+
+    if (classe < (*l)->classe) {
+        log_registrar(&(*l)->esq, conta, classe, tempo, caixa);
+    } else {
+        log_registrar(&(*l)->dir, conta, classe, tempo, caixa);
+    }
+}
+
+int log_obter_soma_por_classe(Log **l, int classe) {
+    if (*l == NULL) return 0;
+
+    int soma = 0;
+
+    if ((*l)->classe == classe) {
+        soma += (*l)->tempo;
+    }
+
+    soma += log_obter_soma_por_classe(&(*l)->esq, classe);
+    soma += log_obter_soma_por_classe(&(*l)->dir, classe);
+
+    return soma;
+}
+
+int log_obter_contagem_por_classe(Log **l, int classe) {
+    if (*l == NULL) return 0;
+
+    int cont = 0;
+
+    if ((*l)->classe == classe) {
+        cont = 1;
+    }
+
+    cont += log_obter_contagem_por_classe(&(*l)->esq, classe);
+    cont += log_obter_contagem_por_classe(&(*l)->dir, classe);
+
+    return cont;
+}
+
+float log_media_por_classe(Log **l, int classe) {
+    int soma = log_obter_soma_por_classe(l, classe);
+    int qtd  = log_obter_contagem_por_classe(l, classe);
+
+    if (qtd == 0) return 0.0;
+
+    return (float) soma / qtd;
+}
+
+void log_liberar(Log *l) {
+    if (l == NULL) return;
+
+    log_liberar(l->esq);
+    log_liberar(l->dir);
+
+    free(l);
+}
--- logtree.h
+++ logtree.h
@@ -1,25 +1,20 @@
+#ifndef LONGTREE_H
+#define LONGTREE_H
 
-
-#ifndef LOGTREE_H
-#define LOGTREE_H
-
-typedef struct node {
-    int conta; 
+typedef struct log {
+    int conta;
     int classe;
-    int timer; 
-    int caixa;
     int tempo;
-    struct node *esq, *dir;
-} Node;
-
-typedef struct {
-    Node *raiz;
+    int caixa;
+    struct log *esq;
+    struct log *dir;
 } Log;
 
-void LogInicializar(Log **I);
-void LogRegistrar(Log **I, int conta, int classe, int timer, int caixa, int tempo);
-float LogMediaPorClasse(Log **I, int classe);
-int LogPegaSomaPorClasse(Log **I, int classe);    
-int LogPegaContagemPorClasse(Log **I, int classe); 
+void log_inicializar(Log **l);
+void log_registrar(Log **l, int conta, int classe, int tempo, int caixa);
+int  log_obter_soma_por_classe(Log **l, int classe);
+int  log_obter_contagem_por_classe(Log **l, int classe);
+float log_media_por_classe(Log **l, int classe);
+void log_liberar(Log *l);
 
 #endif
